5161b2ac3bf06e3563245b2e90326f0e567c7e97
panjie <3792535@qq.com>
2020-08-01 09:48:51 +0800

    init

---
+++ src/main/java/club/codedemo/Criteria/API/An/Example/of/IN/Expressions/Application.java
@@ -0,0 +1,13 @@
+package club.codedemo.Criteria.API.An.Example.of.IN.Expressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/club/codedemo/Criteria/API/An/Example/of/IN/Expressions/ApplicationTests.java
@@ -0,0 +1,13 @@
+package club.codedemo.Criteria.API.An.Example.of.IN.Expressions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
